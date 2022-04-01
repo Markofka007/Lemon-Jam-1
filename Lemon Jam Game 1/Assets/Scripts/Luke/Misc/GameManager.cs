eb41b5d8cfe80cbdd681d979a21dde7de8e7aa92
SirNugget13 <68026241+SirNugget13@users.noreply.github.com>
2022-04-01 10:13:19 -0700

    yert

--- Lemon Jam Game 1/Assets/Scripts/Luke/Misc/GameManager.cs
+++ Lemon Jam Game 1/Assets/Scripts/Luke/Misc/GameManager.cs
@@ -6,6 +6,10 @@ public class GameManager : MonoBehaviour
 {
     public PlatformSpawnManager PSM;
     public GameObject GoBack;
+    public GameObject Player1;
+    public GameObject Player2;
+    public GameObject Player3;
+    public GameObject Player4;
 
     public float Velo = 7;
     public float FinalX = 95;
@@ -13,7 +17,6 @@ public class GameManager : MonoBehaviour
     public GameObject platformSegment;
 
     private bool oneSpawn;
-    private bool oneParent;
     private int i = 0;
 
     // Update is called once per frame
@@ -27,8 +30,10 @@ public class GameManager : MonoBehaviour
         if(GoBack.transform.position.x >= FinalX - 0.1f && oneSpawn)
         {
             platformSegment.transform.parent = GoBack.transform;
+            ParentPlayer();
             GoBack.transform.SetPositionAndRotation(Vector3.zero, Quaternion.identity);
             platformSegment.transform.parent = null;
+            DeParentPlayer();
 
             platformSegment = PSM.platforms[Random.Range(0, PSM.platforms.Length)];
             platformSegment = PSM.SpawnPlatform(platformSegment);
@@ -41,5 +46,21 @@ public class GameManager : MonoBehaviour
 
         GoBack.transform.position += Vector3.right * Velo * Time.deltaTime;
 
+    }
+
+    void ParentPlayer()
+    {
+        if(Player1 != null) { Player1.transform.parent = GoBack.transform; }
+        if(Player2 != null) { Player2.transform.parent = GoBack.transform; }
+        if(Player3 != null) { Player3.transform.parent = GoBack.transform; }
+        if(Player4 != null) { Player4.transform.parent = GoBack.transform; }
+    }
+
+    void DeParentPlayer()
+    {
+        if (Player1 != null) { Player1.transform.parent = null; }
+        if (Player2 != null) { Player2.transform.parent = null; }
+        if (Player3 != null) { Player3.transform.parent = null; }
+        if (Player4 != null) { Player4.transform.parent = null; }
     }
 }
