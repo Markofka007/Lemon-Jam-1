509b246f116c66a7317a240e945223f64bed16f6
Mark <69377981+TheBigCarrot@users.noreply.github.com>
2022-04-22 09:20:46 -0700

    Added bean hovering

--- Lemon Jam Game 1/Assets/Scripts/Mark/Items/Hover.cs
+++ Lemon Jam Game 1/Assets/Scripts/Mark/Items/Hover.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Hover : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float totalTime;
+
+    private Vector3 initialPos;
+
     void Start()
     {
-        
-    }
+        initialPos = transform.position;
 
-    // Update is called once per frame
+        totalTime = Random.Range(0f, 1f);
+    }
+    
     void Update()
     {
-        
+        totalTime += Time.deltaTime;
+
+        Debug.Log(totalTime);
+
+        transform.position =  initialPos + new Vector3(0, Mathf.Sin(totalTime * 5f) * 0.25f, 0);
     }
 }
