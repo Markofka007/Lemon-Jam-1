d7d8a85791d6dab3faa67d638bf2351089fe644c
SirNugget13 <68026241+SirNugget13@users.noreply.github.com>
2022-05-10 10:25:34 -0700

    Animation pleaaaassseee

--- Lemon Jam Game 1/Assets/Scripts/Luke/Platforms/BreakawayPlatform.cs
+++ Lemon Jam Game 1/Assets/Scripts/Luke/Platforms/BreakawayPlatform.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class BreakawayPlatform : MonoBehaviour
 {
+    //public Component anim;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if ((collision.gameObject.tag == "Player1") || (collision.gameObject.tag == "Player2") || (collision.gameObject.tag == "Player3") || (collision.gameObject.tag == "Player4"))
         {
+            gameObject.GetComponent<Animator>().Play("Breaking");
+            
             this.Wait(1f, () =>
             {
                 Destroy(gameObject);
