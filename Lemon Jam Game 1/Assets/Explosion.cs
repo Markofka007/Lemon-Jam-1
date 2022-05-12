6dbb3d323c335e31b71529856ea2fadbe30e705d
Patchoco <nyankosan830@gmail.com>
2022-05-11 18:33:04 -0700

    Explosions!!!!

---
+++ Lemon Jam Game 1/Assets/Explosion.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.Wait(0.7f, () =>
+        {
+            Destroy(gameObject);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
